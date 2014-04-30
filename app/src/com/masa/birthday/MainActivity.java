13374ae6149e6ec38f71627850b4d6639bb86e15
Alina Shnaider <alinashn@localhost>
2014-04-30 15:16:49 +0300

    My first commit.

--- app/src/com/masa/birthday/MainActivity.java
+++ app/src/com/masa/birthday/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		// Alina was here.
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment())
 					.commit();
