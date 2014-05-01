0c58241319309f2606f469bb3dd641d6f365b814
Sion Schori <sion.schori@gmail.com>
2014-05-01 14:42:00 +0300

    Send email from main activity.

--- app/src/com/masa/birthday/MainActivity.java
+++ app/src/com/masa/birthday/MainActivity.java
@@ -10,7 +10,6 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -23,6 +22,8 @@ import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.masa.notifier.Email;
 
 public class MainActivity extends Activity {
 	ListView listView;
@@ -51,8 +52,6 @@ public class MainActivity extends Activity {
 				openBirthdayEditor();
 				v.setEnabled(true);
 			}
-
-			
 		});
 
 		listView = (ListView) findViewById(R.id.birthdays);
@@ -60,12 +59,14 @@ public class MainActivity extends Activity {
 				new BirthdayRecord(
 						"sions",
 						new GregorianCalendar(1984,2,6).getTime(),
-						"placeholdeer");
+						"placeholdeer",
+						"sion@masa.com");
 		BirthdayRecord miris =
 				new BirthdayRecord(
 						"miris",
 						new GregorianCalendar(1984,2,14).getTime(),
-						"placeholdeer");
+						"placeholdeer",
+						"miri@masa.com");
 		
 		birthdays = new ArrayList<BirthdayRecord>();
 		birthdays.add(miris);
@@ -75,24 +76,18 @@ public class MainActivity extends Activity {
 		
 		// To register the button with context menu.
 		registerForContextMenu(listView);
-		//listView.setOnItemClickListener((OnItemClickListener) this);
-		//openContextMenu(listView);
 	}
 	private void openBirthdayEditor() {
-		Intent intent = new Intent(MainActivity.this,
-				EditBirthdayActivity.class);
+		Intent intent = new Intent(MainActivity.this, EditBirthdayActivity.class);
 		startActivity(intent);
 	}
 
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenu.ContextMenuInfo menuInfo) {
-		
 		super.onCreateContextMenu(menu, v, menuInfo);  
         MenuInflater m = getMenuInflater();  
         m.inflate(R.menu.birthday_list_menu, menu); 
-        
-
 	}
 
 	@Override
@@ -103,7 +98,12 @@ public class MainActivity extends Activity {
         		
 		switch (item.getItemId()) {
 		case R.id.send: {
-			//TODO(sions): Do you magic.
+			BirthdayRecord record = birthdays.get(position);
+			String subject = getResources().getString(R.string.happy_birthday_email_title,  
+					record.getName());
+			String text =  getResources().getString(R.string.happy_birthday_email_text);
+			Email email = new Email(record.getEmail(), subject, text, record.getName());
+			startActivity(email.createIntent());
 		}
 			break;
 		case R.id.edit: {
@@ -112,7 +112,6 @@ public class MainActivity extends Activity {
 		}
 			break;
 		case R.id.delete: {
-			
             birthdays.remove(position);  
             this.adapter.notifyDataSetChanged();  
 		}
