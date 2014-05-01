7d64e01dec972baff85ac4806a675dd9a88b63bb
Alina Shnaider <alinashn@localhost>
2014-05-01 09:56:29 +0300

    Initial main activity, db, birthday record. And deleted generated files.

--- app/gen/com/masa/birthday/R.java
+++ app/gen/com/masa/birthday/R.java
@@ -25,8 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080003;
+        public static final int add=0x7f080002;
         public static final int container=0x7f080000;
+        public static final int today=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -37,6 +39,7 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
+        public static final int add=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
     }
@@ -67,5 +70,8 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        /**   TODO(alinashn): Style the add button. 
+         */
+        public static final int addButton=0x7f060002;
     }
 }
